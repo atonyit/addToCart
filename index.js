c446bf49aee5e856973e3698d98a6644070558ed
Alan <alanitoyah96@gmail.com>
2024-05-13 14:54:33 -0500

    small css styling and added event listener/console log out value

---
+++ index.js
@@ -0,0 +1,7 @@
+const addBTN = document.getElementById("add-btn");
+const inputField = document.getElementById("input-field")
+
+
+addBTN.addEventListener("click", function(){
+    console.log(inputField.value);
+})
