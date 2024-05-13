4c531aae535904ffece21668f930f5a3a44bc340
Alan <alanitoyah96@gmail.com>
2024-05-13 16:00:33 -0500

    database

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://playground-5b376-default-rtdb.firebaseio.com/"
 }
-const app = initializeApp(appSettings)
-
-console.log(app)
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
 
 const addBTN = document.getElementById("add-btn");
 const inputField = document.getElementById("input-field")
