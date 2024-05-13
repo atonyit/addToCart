a107b5aae4087870b61dde7c1a4e3a2b6e0c1bcc
Alan <alanitoyah96@gmail.com>
2024-05-13 16:19:13 -0500

    html cleanup and finished setting up database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push } from
+ "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://playground-5b376-default-rtdb.firebaseio.com/"
+    databaseURL: "https://realtime-database-208e0-default-rtdb.firebaseio.com/"
 }
+
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList")
 
 const addBTN = document.getElementById("add-btn");
 const inputField = document.getElementById("input-field")
@@ -14,5 +17,7 @@ const inputField = document.getElementById("input-field")
 addBTN.addEventListener("click", function(){
     let inputValue = inputField.value;
 
-    console.log(inputValue);
+    push(shoppingListInDB, inputValue)
+
+    console.log( `${inputValue} added to database`);
 })
