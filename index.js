c7f50c24029b98f33c76013416151777a0664791
Alan <alanitoyah96@gmail.com>
2024-05-13 15:56:24 -0500

    Changed js type to module imported initializeApp

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
 const appSettings = {
     databaseURL: "https://playground-5b376-default-rtdb.firebaseio.com/"
 }
+const app = initializeApp(appSettings)
+
+console.log(app)
 
 const addBTN = document.getElementById("add-btn");
 const inputField = document.getElementById("input-field")
