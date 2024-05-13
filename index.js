bb70831c109d1ad04b861da2f20af481a28ab184
Alan <alanitoyah96@gmail.com>
2024-05-13 15:48:26 -0500

    Input firebase database url

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const appSettings = {
+    databaseURL: "https://playground-5b376-default-rtdb.firebaseio.com/"
+}
+
 const addBTN = document.getElementById("add-btn");
 const inputField = document.getElementById("input-field")
 
 
 addBTN.addEventListener("click", function(){
     let inputValue = inputField.value;
-    
+
     console.log(inputValue);
 })
